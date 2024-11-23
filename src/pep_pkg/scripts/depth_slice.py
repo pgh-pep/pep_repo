97c28970cd5faa48e3693105cbfc40d99222e9eb
Jack8701 <jcarluccio2006@gmail.com>
2024-11-23 07:29:23 -0500

    working depth slice

--- src/pep_pkg/scripts/depth_slice.py
+++ src/pep_pkg/scripts/depth_slice.py
@@ -1,46 +1,41 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from interfaces.msg import DepthSlice
-from interfaces.msg import BuoyLocation
+from interfaces.msg import DepthSlice as DepthSliceMsg
+from interfaces.msg import BuoyLocation as BuoyLocationMsg
 from interfaces.srv import GetBuoyLocation
 
 class DepthSlice(Node):
     def __init__(self):
         super().__init__('depth_slice')
 
-        self.publisher = self.create_publisher(DepthSlice, 'depth_slice', 10)
-        self.subscriber = self.create_subscription(BuoyLocation, 'process_depth_slice', self.process_depth_slice_callback, 10)
+        self.publisher = self.create_publisher(DepthSliceMsg, 'depth_slice', 10)
+        self.subscriber = self.create_subscription(BuoyLocationMsg, 'process_depth_slice', self.process_depth_slice_callback, 10)
 
         self.buoy_detection_client = self.create_client(GetBuoyLocation, 'get_buoy_location')
         self.buoy_detection_client.wait_for_service()
         self.request = GetBuoyLocation.Request()
 
 
-    def process_depth_slice_callback(self, msg):
-        print("Processing depth slice...", msg)
-        slice = [-1] * 1280
+    def process_depth_slice_callback(self, buoy_location):
+        print("Processing depth slice...", buoy_location)
+        slice = [-1.0] * 1280
 
         # Populates the slice with the depth of the buoy, wherever it is
-        left, right = max(0, msg.data.left), min(1280 - 1, msg.data.left + msg.data.width)
+        left, right = max(0, buoy_location.left), min(1280 - 1, buoy_location.left + buoy_location.width)
         for i in range(left, right):
-            slice[i] = 1
+            slice[i] = 1.0
 
-        msg = DepthSlice()
-        msg.data = slice
+        msg = DepthSliceMsg()
+        msg.depth_slice = slice
         self.publisher.publish(msg)
-        print(slice)
-
-        return slice
 
 
 def main():
     rclpy.init()
 
     depth_slice = DepthSlice()
-
-    depth_slice.get_depth_slice()
-
+    print("Yeah we up")
     rclpy.spin(depth_slice)
 
     depth_slice.destroy_node()
--- src/pep_pkg/scripts/main.py
+++ src/pep_pkg/scripts/main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from interfaces.msg import BuoyLocation
+from interfaces.msg import BuoyLocation as BuoyLocationMsg
 from interfaces.srv import GetBuoyLocation
 
 class Main(Node):
     def __init__(self):
         super().__init__('main')
 
-        self.process_depth_slice_publisher = self.create_publisher(BuoyLocation, 'process_depth_slice', 10)
+        self.process_depth_slice_publisher = self.create_publisher(BuoyLocationMsg, 'process_depth_slice', 10)
 
         self.buoy_detection_client = self.create_client(GetBuoyLocation, 'get_buoy_location')
         self.buoy_detection_client.wait_for_service()
@@ -21,12 +21,13 @@ class Main(Node):
         buoy_location_response = buoy_location_future.result()
 
         # Convert the service response to a message, because ros hates me. Surely there's a better way to do this.
-        buoy_location_message = BuoyLocation()
+        buoy_location_message = BuoyLocationMsg()
         buoy_location_message.left = buoy_location_response.left
         buoy_location_message.top = buoy_location_response.top
         buoy_location_message.width = buoy_location_response.width
         buoy_location_message.height = buoy_location_response.height
 
+        print("published")
         self.process_depth_slice_publisher.publish(buoy_location_message)
 
 
@@ -35,6 +36,7 @@ def main():
 
     main = Main()
     main.foo()
+    print("post foo")
 
     rclpy.spin(main)
     rclpy.shutdown()
